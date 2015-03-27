4bbd9deec31a99a185b9a7aa5d0133f83cfaa661
Mau Rodríguez Morales <maUCRodriguez@gmail.com>
2015-03-27 16:06:50 -0600

    Spidey Crawler has been redifined

--- spidey/crawler.php
+++ spidey/crawler.php
@@ -1,18 +1,25 @@
 <?php
-	$url = trim($argv[1]);
+	error_reporting(E_ERROR | E_PARSE);
 
+	$url = trim($argv[1]);
 	$md5 = md5($url);
 	$one = substr($md5,0,2);
 	$two = substr($md5,2,2);
+	if(!file_exists('./documents/')){
+		mkdir('./documents/');
+	}
 
 	if(!file_exists('./documents/'.$one.'/'.$two.'/'.$md5)) {
-		echo 'Downloading - '.$url."\r\n";
+//		echo 'Downloading - '.$url."\r\n";
 		$content = file_get_contents($url);
 		$document = array($url,$content);
 		$serialized = serialize($document);
-		mkdir('./documents/');
-		mkdir('./documents/'.$one.'/');
-		mkdir('./documents/'.$one.'/'.$two.'/');
+		if(!file_exists('./documents/'.$one.'/')){
+			mkdir('./documents/'.$one.'/');
+		}
+		if(!file_exists('./documents/'.$one.'/'.$two.'/')){
+			mkdir('./documents/'.$one.'/'.$two.'/');
+		}
 		$fp = fopen('./documents/'.$one.'/'.$two.'/'.$md5, 'w');
 		fwrite($fp, $serialized);
 		fclose($fp);
